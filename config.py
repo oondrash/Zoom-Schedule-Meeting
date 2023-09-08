d70748e660a933de2dca70a86bea53fece97eae9
Andrii Nazarenko <nazarenkoandriy3@gmail.com>
2023-09-08 15:25:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 schedule = [
-    ["https://us04web.zoom.us/j/79499259796?pwd=KWaPbYhVtiB6VYvUqJjsbEGyLLD6jj.1", "18:57", "19:58"],
+    ["https://us06web.zoom.us/j/88993228907?pwd=aVU2UnN3MTdQVEE3RTdJMk0wQTFiUT09", "10:35", "12:05"],
 ]
 
 user_ids = [
@@ -7,9 +7,10 @@ user_ids = [
     579536517
 ]
 
-TOKEN = 'MyToken'
+TOKEN = '6317041667:AAHlyg-wS9acRvRJ2c-WNffoZZhDbYSRT1I'
 NAME = "Андрій"
 SURNAME = "Назаренко"
 GROUP = "ДО-4"
 CHAT_KEY = str(9)
 LEAVE_KEY = str(8)
+FULL_SCREEN_KEY = str(7)
